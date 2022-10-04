cd197083ae334970f053978f9552edce1dfe53ca
Balazs Tieger <btieger@rapidminer.com>
2022-10-04 18:33:59 +0200

    impl db source

--- src/parsers/db_parser.py
+++ src/parsers/db_parser.py
@@ -5,13 +5,31 @@ import uuid
 import os
 
 def parse(file: UploadedFile, args: dict[str, str]) -> tuple[list[str], list[list[str]]]:
+    table = args.get('from')
+    columns = args.get('select', '*')
+    query = args.get('query')
+
     try:
+        # TODO: do not write to disk, stream inside memory
         generated_name = f'{uuid.uuid4()}.db'
-        with open(generated_name, 'w') as f:
-            f.write(file.read())
-        
+        data = file.read()
+        with open(generated_name, 'wb') as f:
+            f.write(data)
         conn = connect(generated_name)
-        pd.read_sql('SELECT * from demo;', conn)
-    finally: 
-        conn.close()
-        os.remove(generated_name)
+        
+        if query is None:        
+            if table is None:
+                table = pd.read_sql(
+                    '''SELECT name FROM sqlite_master WHERE type='table';''',
+                    conn
+                ).values[0][0]
+            query = f'SELECT {columns} FROM {table};'
+        
+        # NOTE: This is SQL injection vulnerable, however it poses no threat, since the query is executed on a temporary copy of a user-uploaded database file.
+        parsed = pd.read_sql(query, conn)
+        return [i for i in parsed.columns], [[j for j in i] for i in parsed.values]
+    finally:
+        try:
+            conn.close()
+        finally:
+            os.remove(generated_name)
