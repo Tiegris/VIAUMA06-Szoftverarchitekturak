e52b0602d04ce7f2d7b15dbca5e1832800e9c54b
Balazs Tieger <btieger@rapidminer.com>
2022-10-05 18:23:06 +0200

    abc

--- src/parsing/parser_base.py
+++ src/parsing/parser_base.py
@@ -3,10 +3,12 @@ from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from .help import Help
 
-class ParserBase:
+class ParserBase(ABC):
     
+    @abstractmethod
     def parse(self, file: UploadedFile, args: dict[str, str]) -> tuple[list[str], list[list[str]]]:
         pass
     
+    @abstractmethod
     def help(self) -> list[Help]:
         pass
